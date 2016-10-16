92795d524650ae5292adedd8ec571938259cc6fc
noahmilstein <casanoah@gmail.com>
2016-10-16 15:35:11 -0400

    added admin boolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013000516) do
+ActiveRecord::Schema.define(version: 20161016193254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,29 +27,30 @@ ActiveRecord::Schema.define(version: 20161013000516) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
-    t.string   "username",               default: "", null: false
-    t.string   "phone",                  default: "", null: false
-    t.string   "country",                default: "", null: false
-    t.string   "state",                  default: "", null: false
-    t.string   "city",                   default: "", null: false
-    t.string   "zip",                    default: "", null: false
-    t.boolean  "seeking_coach",                       null: false
-    t.boolean  "accept_email",                        null: false
-    t.boolean  "accept_phone",                        null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "first_name",             default: "",    null: false
+    t.string   "last_name",              default: "",    null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "phone",                  default: "",    null: false
+    t.string   "country",                default: "",    null: false
+    t.string   "state",                  default: "",    null: false
+    t.string   "city",                   default: "",    null: false
+    t.string   "zip",                    default: "",    null: false
+    t.boolean  "seeking_coach",                          null: false
+    t.boolean  "accept_email",                           null: false
+    t.boolean  "accept_phone",                           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
