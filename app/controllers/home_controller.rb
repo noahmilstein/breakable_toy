d2d852fc7659d351f382add18bc530abf30b6d8a
noahmilstein <casanoah@gmail.com>
2016-10-21 17:33:17 -0400

    nav bar is now react

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-  
+    currentUser = current_user.blank?
+    unless current_user.nil?
+      admin = current_user.admin
+    end
+    user_json = { "currentUser": currentUser, "admin": admin }
+    respond_to do |format|
+      format.json { render json: user_json }
+      format.html
+    end
   end
-
 end
