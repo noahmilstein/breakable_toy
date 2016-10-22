a29ae93fab78d58aa667ada49e5e3aa88046163c
noahmilstein <casanoah@gmail.com>
2016-10-22 12:55:37 -0400

    initial add carrierwave, WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021000050) do
+ActiveRecord::Schema.define(version: 20161022161005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20161021000050) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false, null: false
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
---
+++ spec/features/11_carrierwave_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+feature "profile photo" do
+  scenario "user uploads a profile photo" do
+    visit root_path
+    click_link "Sign Up"
+
+    visit new_user_registration_path
+    fill_in "First Name", with: "Steve"
+    fill_in "Last Name", with: "Steve"
+    fill_in "Username", with: "Steve"
+    fill_in "Email", with: "Steve@Steve.com"
+    fill_in "Phone", with: "666-666-6666"
+    attach_file 'Add Avatar', "#{Rails.root}/spec/support/Dr_John_Zoidberg.png"
+    fill_in "Password", with: "password"
+    fill_in "Confirm Password", with: "password"
+    select "NY", from: "State"
+    fill_in "City", with: "Steve"
+    fill_in "Zip", with: "12345"
+    check "Are you seeking a coach?"
+    check "Can our coaches email you?"
+    check "Can our coaches call or text you?"
+    click_button "Sign Up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(page).to have_xpath("//img")
+    expect(page).to have_css("img")
+  end
+end
