fce0903f1f418ad14132fc8bbb2ccc1aeb921feb
noahmilstein <casanoah@gmail.com>
2016-10-25 09:48:37 -0400

    responded to hound errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
 
   resources :users, only: [] do
     collection do
-      get '/show_profile', to: 'users#show_profile', as: 'my_profile'
-      get '/show_log', to: 'users#show_log', as: 'my_log'
-      get '/coach_index', to: 'users#coach_index', as: 'coach_index'
-      get '/trainee_index', to: 'users#trainee_index', as: 'trainee_index'
-      post '/request_admin', to: 'users#request_admin', as: 'request_admin'
+      get "/show_profile", to: "users#show_profile", as: "my_profile"
+      get "/show_log", to: "users#show_log", as: "my_log"
+      get "/coach_index", to: "users#coach_index", as: "coach_index"
+      get "/trainee_index", to: "users#trainee_index", as: "trainee_index"
+      post "/request_admin", to: "users#request_admin", as: "request_admin"
     end
     resources :posts, only: [:new, :create]
   end
 
-  get 'tags/:tag', to: 'videos#index', as: :tag
+  get "tags/:tag", to: "videos#index", as: :tag
 
   resources :posts, except: [:new, :create] do
     resources :videos, except: [:show, :index, :new, :edit, :destroy]
@@ -22,8 +22,8 @@ Rails.application.routes.draw do
   end
   resources :home, only: [:about] do
     collection do
-      get '/about', to: 'home#about', as: 'about'
+      get "/about", to: "home#about", as: "about"
     end
   end
-  root 'home#index'
+  root "home#index"
 end
